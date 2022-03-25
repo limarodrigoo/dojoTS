567439c102fc139e6fc93586b5f56ba2bc887f14
Pedro Ramos <pedro@betrybe.com>
2022-03-25 16:01:08 -0300

    FIX: Add any  typing to handler

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import { UserService } from '../services';
 
 export default class UserController {
-  public async exampleMethod(_req, res) {
+  public async exampleMethod(_req: any, res: any) {
     res.status(StatusCodes.NOT_IMPLEMENTED).end();
   }
 }
